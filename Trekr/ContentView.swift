fc2b35a932b0fb63309c4e220b43100783d9b7e9
dihsarjr <mohammed1rashid786@gmail.com>
2023-11-04 19:15:44 +0530

    Initial Commit

---
+++ Trekr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by Mohammed Rashid on 04/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Mohammed Rashid on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
